59b5ae5f5dd6c033c06d6e5d9f30b2edecdc7c6b
VITOR MARCELO HONORIO <vitor.honorio@bradesco.com.br>
2020-05-04 18:53:29 -0300

    Automação do Build via Webhooks

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'Segunda...versão do Vitor Honório !!' + '\n';
+    response = 'Teceira versão do Vitor Honório com automação do build !!' + '\n';
 
     //send the response to the client
     res.send(response);
